facfcb2c30bf7330ec0687d9248283f4648831fe
Aitor Perez <aitorpegea@gmail.com>
2020-05-09 22:37:15 +0200

    Añadida la suma de 10 y 12

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def main():
     print('Hola Laura')
+    print('La suma de 10 y 12 es {}'.format(calculaSuma(10, 12)))
+
+def calculaSuma(x, y):
+    return x + y
 
 if __name__ == '__main__':
     main()
