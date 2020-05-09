d7626d1d1aa05822b6a302dcf79134761254c7ee
Aitor Perez <aitorpegea@gmail.com>
2020-05-09 22:34:05 +0200

    Added Aitor and calculandoCosas

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def main():
     print('Hola Laura')
+    print('Hola Aitor')
+    x = calculandoCosas()
+    print(x)
+
+def calculandoCosas():
+    return 5
 
 if __name__ == '__main__':
     main()
